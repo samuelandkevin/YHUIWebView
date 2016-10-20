529bf3bcb3cb06a2654d451530ba2d18851a6bcb
samuelandkevin <YHIOS002@samuelandkevin.local>
2016-10-20 10:09:25 +0800

    Initial Commit

---
+++ YHUIWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHUIWebView
+//
+//  Created by YHIOS002 on 16/10/20.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YHUIWebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YHUIWebView
+//
+//  Created by YHIOS002 on 16/10/20.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
